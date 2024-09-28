9e38121ec7b630a5f869ffd98d2190e035c6dc15
Duzie <duzzi4x@gmail.com>
2024-09-28 17:18:43 +0200

    Expanded data stucture to include additional props

--- models/software_tasks.py
+++ models/software_tasks.py
@@ -4,34 +4,34 @@ from pydantic import BaseModel
 class SoftwareTasks(BaseModel):
 
     ## Backend tasks
-    database_task: str
-    security_task: str
-    validation_task: str
-    dev_ops_task: str
-    server_management_task: str
-    api_setup_task: str
-    api_integration_task: str
-    data_backup_task: str
-    backend_testing_task: str
-    data_structure_task: str
-    machine_learning_task: str
-    scalability_task: str
-    optimization_task: str
-    cloud_task: str
+    database_task: dict[str, str]
+    security_task: dict[str, str]
+    validation_task: dict[str, str]
+    dev_ops_task: dict[str, str]
+    server_management_task: dict[str, str]
+    api_setup_task: dict[str, str]
+    api_integration_task: dict[str, str]
+    data_backup_task: dict[str, str]
+    backend_testing_task: dict[str, str]
+    data_structure_task: dict[str, str]
+    machine_learning_task: dict[str, str]
+    scalability_task: dict[str, str]
+    optimization_task: dict[str, str]
+    cloud_task: dict[str, str]
 
     ## Frontend tasks
-    styling_task: str
-    ui_ux_task: str
-    frontend_testing_task: str
-    api_logic_task: str
-    form_setup_task: str
-    table_setup_task: str
-    layout_setup_task: str
-    data_display_task: str
-    data_visualization_task: str
-    access_control_task: str
-    seo_task: str
-    widget_setup_task: str
-    ci_cd_task: str
-    deployment_task: str
-    cms_integration_task: str
+    styling_task: dict[str, str]
+    ui_ux_task: dict[str, str]
+    frontend_testing_task: dict[str, str]
+    api_logic_task: dict[str, str]
+    form_setup_task: dict[str, str]
+    table_setup_task: dict[str, str]
+    layout_setup_task: dict[str, str]
+    data_display_task: dict[str, str]
+    data_visualization_task: dict[str, str]
+    access_control_task: dict[str, str]
+    seo_task: dict[str, str]
+    widget_setup_task: dict[str, str]
+    ci_cd_task: dict[str, str]
+    deployment_task: dict[str, str]
+    cms_integration_task: dict[str, str]
